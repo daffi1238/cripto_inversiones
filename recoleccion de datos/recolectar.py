2f5a266931491bc65dbb943c74ca78492592b0eb
daffi1238 <jazz.sumariva@gmail.com>
2021-01-20 16:14:22 +0100

    La recolección ahora toma el valor medio del precio en un minuto y os valores máximos y minimos de ese mismo minuto

--- recoleccion de datos/recolectar.py
+++ recoleccion de datos/recolectar.py
@@ -15,19 +15,33 @@ for x in range(10):#10 minutos
     minuto=datetime.now().minute
     valor_btc=[]
 
-    i=1
+    i=0
+    precio_max=0
+    precio_min=0
     while i<60:  #Lo idoneo sería 60, pero por condición de carrera pudiera fallar
         now = datetime.now()
+        #Añadimos a los datos el valor máximo y el mínimo
+        precio=cryptocompare.get_price('BTC')['BTC']['EUR']
+        if precio_max==0:
+            precio_max=precio
+            precio_min=precio
+
+        elif precio_max<precio:
+            precio_max=precio
+        elif precio_min>precio:
+            precio_min=precio
+        else:
+            print("No se modifica nada")
+
         if(now.minute==minuto):
             print("El valor de minuto: " + str(now.minute))
-            #print("El valor de la moneda " + str(cryptocompare.get_price('BTC')['BTC']['EUR'])
-            valor_btc.append(cryptocompare.get_price('BTC')['BTC']['EUR'])
+            valor_btc.append(precio)
         else:
             print("El valor de minuto dentro del else: " + str(now.minute))
             print("El valor de i "+str(i) + "El valor del sumatorio: "+str(sum(valor_btc)))
             media_minuto=np.sum(valor_btc)/i
             fecha=str(now.year) +'/'+str(now.month)+'/'+str(now.day)+'/'+str(now.minute)
-            csvWriter.writerow( [fecha, media_minuto] )
+            csvWriter.writerow( [fecha, media_minuto, precio_max, precio_min] )
 
             print("El valor de la media en el minuto " + str(now.minute) +" es " + str(media_minuto))
 
